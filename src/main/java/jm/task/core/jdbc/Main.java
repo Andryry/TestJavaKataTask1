fe9c8c6703ef10f002ba70e6762b69be756ccb23
Andry <kaplin1998@list.ru>
2026-03-08 19:48:03 +0300

    Edit UserServiceImpl

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -7,13 +7,7 @@ import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
-        UserService userService = new UserServiceImpl();
-        try {
-            userService.createUsersTable();
-//            userService.dropUsersTable();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+
     }
 
 }
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 
@@ -8,26 +9,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    public void createUsersTable() throws SQLException {
+
+    private UserDaoJDBCImpl userDaoJDBC;
+
+    public void createUsersTable() {
+        userDaoJDBC.createUsersTable();
     }
 
 
-    public void dropUsersTable() throws SQLException {
-
+    public void dropUsersTable() {
+        userDaoJDBC.dropUsersTable();
     }
 
-    public void saveUser(String name, String lastName, byte age) throws SQLException {
-
+    public void saveUser(String name, String lastName, byte age) {
+        userDaoJDBC.saveUser(name, lastName, age);
     }
 
-    public void removeUserById(long id) throws SQLException {
-
+    public void removeUserById(long id) {
+        userDaoJDBC.removeUserById(id);
     }
 
-    public List<User> getAllUsers() throws SQLException {
-        return null;
+    public List<User> getAllUsers() {
+        return userDaoJDBC.getAllUsers();
     }
 
-    public void cleanUsersTable() throws SQLException {
+    public void cleanUsersTable() {
+        userDaoJDBC.cleanUsersTable();
     }
 }
